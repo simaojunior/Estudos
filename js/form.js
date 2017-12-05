96e469aacfaa0b8b600234297199e068d244be24
Simão Junior <sima.msjr@gmail.com>
2017-12-05 17:33:55 -0200

    Create form.

---
+++ js/form.js
@@ -0,0 +1,61 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+  // console.log("fui clicado");
+  var form = document.querySelector("#form-adiciona");
+  // extraindo informacções do paciente.
+  var paciente = obtemPacienteDoFormulario(form);
+  //cria a Tr e a Td DO Paciente.
+  var pacienteTr = montaTr(paciente);
+
+  if (!validaPaciente(paciente)) {
+    console.log("paciente inválido");
+    return;
+  }
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+
+  form.reset();
+});
+
+function obtemPacienteDoFormulario(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value)
+  }
+
+  return paciente;
+}
+
+function montaTr(paciente) {
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+  return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+
+  return td;
+}
+
+function validaPaciente(paciente) {
+  if (validaPeso(paciente.peso)) {
+    return true;
+  } else {
+    return false;
+  }
+}
