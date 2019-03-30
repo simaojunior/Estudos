63055214a4212b210776c356f47355156377627c
Simão Junior <simao.msjr@gmail.com>
2019-03-30 16:36:05 -0300

    Atualizando o código para ES8.

---
+++ js/remover-paciente.js
@@ -0,0 +1,10 @@
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", (event) => {
+  event.target.parentNode.classList.add("fadeout");
+
+  setTimeout(function () {
+    event.target.parentNode.remove();
+  },500);
+
+});
